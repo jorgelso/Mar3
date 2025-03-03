233e29bd9aaa7e720508ff37e24f9f212032de5f
Jorge Salcedo <165866373+jorgelso@users.noreply.github.com>
2025-03-03 12:41:26 -0600

    fetch results

---
+++ src/app/Hooks/useFetchData.tsx
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+
+const API_URL = "https://randomuser.me/api/";
+
+interface Person {
+    gender: string;
+    name: {
+        title: string;
+        first: string;
+        last: string;
+    };
+    location: {
+        street: {
+            number: number;
+            name: string;
+        };
+        city: string;
+        state: string;
+        country: string;
+        postcode: string | number;
+        coordinates: {
+            latitude: string;
+            longitude: string;
+        };
+        timezone: {
+            offset: string;
+            description: string;
+        };
+    };
+    email: string;
+    login: {
+        uuid: string;
+        username: string;
+        password: string;
+        salt: string;
+        md5: string;
+        sha1: string;
+        sha256: string;
+    };
+    dob: {
+        date: string;
+        age: number;
+    };
+    registered: {
+        date: string;
+        age: number;
+    };
+    phone: string;
+    cell: string;
+    id: {
+        name: string;
+        value: string;
+    };
+    picture: {
+        large: string;
+        medium: string;
+        thumbnail: string;
+    };
+    nat: string;
+}
+
+export const useFetchData = () => {
+  const [data, setData] = useState<Person | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const response = await fetch(`${API_URL}`);
+        
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const json: Person = await response.json();
+        setData(json);
+      } catch (err) {
+        setError((err as Error).message);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  },);
+
+  return { data, loading, error };
+};
